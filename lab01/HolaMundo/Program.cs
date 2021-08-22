b87b64e435e9e6327c7927d56be32cdd0550ae98
DiegoPamo <diego.pamo@tecsup.edu.pe>
2021-08-22 12:14:16 -0500

    lab01 - parte 1

---
+++ lab01/HolaMundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola como te llamas??");
+            String nombre =  Console.ReadLine();
+            Console.WriteLine("AEA este gil se llama : "+ nombre );
+        }
+    }
+}
