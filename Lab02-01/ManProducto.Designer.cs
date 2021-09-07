47169d84c5828a68ca7c0697230ed5001e894b8e
DiegoPamo <diego.pamo@tecsup.edu.pe>
2021-09-07 18:04:37 -0500

    union de lab02 y 03

---
+++ Lab02-01/ManProducto.Designer.cs
@@ -0,0 +1,310 @@
+﻿
+namespace Lab02_01
+{
+    partial class ManProducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ManProducto));
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombreProduct = new System.Windows.Forms.TextBox();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPrecioUni = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dptFechaVenc = new System.Windows.Forms.DateTimePicker();
+            this.imgProduct = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCodProduct = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(12, 265);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.RowHeadersWidth = 51;
+            this.dgvProductos.RowTemplate.Height = 24;
+            this.dgvProductos.Size = new System.Drawing.Size(776, 173);
+            this.dgvProductos.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtCodProduct);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.imgProduct);
+            this.panel1.Controls.Add(this.dptFechaVenc);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.txtCategoria);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtPrecioUni);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtStock);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtMarca);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtNombreProduct);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(10, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(777, 210);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(289, 226);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(120, 29);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(415, 226);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(120, 29);
+            this.btnModificar.TabIndex = 3;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(541, 226);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(120, 29);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(667, 226);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(120, 29);
+            this.btnBuscar.TabIndex = 5;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre Producto";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtNombreProduct
+            // 
+            this.txtNombreProduct.Location = new System.Drawing.Point(163, 57);
+            this.txtNombreProduct.Name = "txtNombreProduct";
+            this.txtNombreProduct.Size = new System.Drawing.Size(167, 22);
+            this.txtNombreProduct.TabIndex = 1;
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(531, 27);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(167, 22);
+            this.txtMarca.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(387, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Marca";
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(163, 177);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(167, 22);
+            this.txtStock.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Stock";
+            // 
+            // txtPrecioUni
+            // 
+            this.txtPrecioUni.Location = new System.Drawing.Point(163, 133);
+            this.txtPrecioUni.Name = "txtPrecioUni";
+            this.txtPrecioUni.Size = new System.Drawing.Size(167, 22);
+            this.txtPrecioUni.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Precio c/u";
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.Location = new System.Drawing.Point(163, 96);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(167, 22);
+            this.txtCategoria.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Categoria";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(387, 65);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(116, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Fec. Vencimiento";
+            // 
+            // dptFechaVenc
+            // 
+            this.dptFechaVenc.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dptFechaVenc.Location = new System.Drawing.Point(531, 65);
+            this.dptFechaVenc.Name = "dptFechaVenc";
+            this.dptFechaVenc.Size = new System.Drawing.Size(120, 22);
+            this.dptFechaVenc.TabIndex = 11;
+            // 
+            // imgProduct
+            // 
+            this.imgProduct.Image = ((System.Drawing.Image)(resources.GetObject("imgProduct.Image")));
+            this.imgProduct.Location = new System.Drawing.Point(531, 108);
+            this.imgProduct.Name = "imgProduct";
+            this.imgProduct.Size = new System.Drawing.Size(167, 89);
+            this.imgProduct.TabIndex = 12;
+            this.imgProduct.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(390, 107);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Imagen Referencial";
+            // 
+            // txtCodProduct
+            // 
+            this.txtCodProduct.Location = new System.Drawing.Point(163, 24);
+            this.txtCodProduct.Name = "txtCodProduct";
+            this.txtCodProduct.Size = new System.Drawing.Size(167, 22);
+            this.txtCodProduct.TabIndex = 15;
+            this.txtCodProduct.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(38, 24);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(113, 17);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Codigo Producto";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // ManProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dgvProductos);
+            this.Name = "ManProducto";
+            this.Text = "ManProducto";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgProduct)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvProductos;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtNombreProduct;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPrecioUni;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox imgProduct;
+        private System.Windows.Forms.DateTimePicker dptFechaVenc;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCodProduct;
+        private System.Windows.Forms.Label label8;
+    }
+}
