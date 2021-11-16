c29d466d5b2bc3f367a5bfd853d5d6b21c6dd028
DiegoPamo <diego.pamo@tecsup.edu.pe>
2021-11-16 18:44:30 -0500

    Lab13 Tarea Completa

--- lab13/Controllers/PersonaController.cs
+++ lab13/Controllers/PersonaController.cs
@@ -14,7 +14,7 @@ namespace lab13.Controllers
         {
             return View();
         }
-        public ActionResult Listar()
+        public ActionResult Listar(string campo , string valor)
         {
             List<Persona> personas = new List<Persona>();
             personas.Add(new Persona
@@ -44,7 +44,55 @@ namespace lab13.Controllers
                 FechaNac = Convert.ToDateTime("1982-02-14"),
                 Email = "carlos@mail.com"
             });
-            return View(personas);
+
+            if (!String.IsNullOrEmpty(campo))
+            {
+                return View(personas.Where(s => s.Nombre.StartsWith(valor)).ToList());
+            }
+            else
+            {
+                return View(personas);
+            }
+        }
+
+        public ActionResult Mostrar(int id)
+        {
+            List<Persona> personas = new List<Persona>();
+            personas.Add(new Persona
+            {
+                PersonaID = 1,
+                Nombre = "Juan",
+                Apellido = "Perez",
+                Direccion = "Av. Evergen 123",
+                FechaNac = Convert.ToDateTime("1997-11-07"),
+                Email = "juan@mail.com"
+            });
+
+            personas.Add(new Persona
+            {
+                PersonaID = 2,
+                Nombre = "María",
+                Apellido = "Salas",
+                Direccion = "Av. Progreso 325",
+                FechaNac = Convert.ToDateTime("1995-10-28"),
+                Email = "maria@mail.com"
+            });
+
+            personas.Add(new Persona
+            {
+                PersonaID = 3,
+                Nombre = "Carlos",
+                Apellido = "Martinez",
+                Direccion = "Av. Los manzanos 3101",
+                FechaNac = Convert.ToDateTime("1982-02-14"),
+                Email = "carlos@mail.com"
+            });
+
+            Persona persona = (from p in personas
+                               where p.PersonaID == id
+                               select p).FirstOrDefault();
+
+            return View(persona);
         }
     }
 }
